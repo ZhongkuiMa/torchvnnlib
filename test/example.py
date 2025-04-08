3da038f6decc43eca23a85ed371cc63335018259
ZhongkuiMa <zhongkuima0419@gmail.com>
2025-04-08 13:45:12 +1000

    Update file structure

--- test/example.py
+++ test/example.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
 
     converter = TorchVNNLIB(verbose=True)
 
-    file_path = "prop_6.vnnlib"
+    file_path = "../data/prop_6.vnnlib"
     converter.convert(file_path)
     data_path = file_path.replace(".vnnlib", ".pth")
     t = time.perf_counter()
@@ -16,7 +16,8 @@ if __name__ == "__main__":
     t = time.perf_counter() - t
     print(f"Loaded {data_path} ({t:.4f}s)")
 
-    file_path = "TinyImageNet_resnet_medium_prop_idx_57_sidx_9858_eps_0.0039.vnnlib"
+    file_path = (
+        "../data/TinyImageNet_resnet_medium_prop_idx_57_sidx_9858_eps_0.0039.vnnlib")
     converter.convert(file_path)
     data_path = file_path.replace(".vnnlib", ".pth")
     t = time.perf_counter()
